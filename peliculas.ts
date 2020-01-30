a87e26e51add5c62c72bae36af7190997e7a1899
unknown <geafalconjoseantonio@gmail.com>
2020-01-30 16:21:16 +0100

    Cambios peliculas

--- peliculas.ts
+++ peliculas.ts
@@ -52,3 +52,23 @@ export class Peliculas {
     }
 
 }
+let pelicula1: Peliculas;
+pelicula1 = new Peliculas("Vengadores", 2012, "Estados Unidos")
+let pelicula2: Peliculas;
+pelicula2 = new Peliculas("American Beauty", 2000, "Estados unidos")
+let pelicula3: Peliculas;
+pelicula3 = new Peliculas("Rock'n rolla", 2009, "Reino unido")
+let pelicula4: Peliculas;
+pelicula4 = new Peliculas("Mistic river", 2003, "Estados unidos")
+let pelicula5: Peliculas;
+pelicula5 = new Peliculas("Sin perdon ", 1992, "Estados Unidos")
+let pelicula6: Peliculas;
+pelicula6 = new Peliculas("Terminator", 1985, "Estados unidos")
+let pelicula7: Peliculas;
+pelicula7 = new Peliculas("Yo , yo mismo e Irene", 1985, "Estados unidos")
+let pelicula8: Peliculas;
+pelicula8 = new Peliculas("El show de Truman", 1998, "Estados unidos")
+let pelicula9: Peliculas;
+pelicula9 = new Peliculas("Star wars", 1977, "Una galaxia muy muy lejana")
+let pelicula10: Peliculas;
+pelicula10 = new Peliculas("Iron man", 2000, "Estados unidos")
